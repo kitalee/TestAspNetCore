1133bd0f240baac91acfc4a0d7cc916521c525b3
DongGeon Lee <donggeon.lee@keymedia.com>
2019-07-24 13:26:11 +1000

    test

--- TestAspNetCore/Pages/Index.cshtml.cs
+++ TestAspNetCore/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,15 @@ namespace TestAspNetCore.Pages
 {
     public class IndexModel : PageModel
     {
+        public IHostingEnvironment _env;
+
+        public IndexModel(IHostingEnvironment env)
+        {
+            _env = env;
+        }
         public void OnGet()
         {
-
+            ViewData["envName"] = _env.EnvironmentName;
         }
     }
 }
